dd0163891bc6cfa461b277fdc073863438889c3f
Jyoti Prakash <jpjangid710@gmail.com>
2026-04-29 01:11:07 +0530

    Time: 77 ms (48.44%), Space: 93.8 MB (65.21%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,17 +1,38 @@
+// class Solution {
+// public:
+//    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+//     unordered_map<int, int> mp; // value → last index
+
+//     for(int i = 0; i < nums.size(); i++) {
+//         if(mp.find(nums[i]) != mp.end()) {
+//             if(abs(i - mp[nums[i]]) <= k) {
+//                 return true;
+//             }
+//         }
+//         mp[nums[i]] = i; // update latest index
+//     }
+
+//     return false;
+// }
+// };
+//sliding.  window---.>>>>>>
 class Solution {
 public:
-   bool containsNearbyDuplicate(vector<int>& nums, int k) {
-    unordered_map<int, int> mp; // value → last index
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> seen;
 
-    for(int i = 0; i < nums.size(); i++) {
-        if(mp.find(nums[i]) != mp.end()) {
-            if(abs(i - mp[nums[i]]) <= k) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > k) {
+                seen.erase(nums[i - k - 1]);
+            }
+
+            if (seen.find(nums[i]) != seen.end()) {
                 return true;
             }
-        }
-        mp[nums[i]] = i; // update latest index
-    }
 
-    return false;
-}
+            seen.insert(nums[i]);
+        }
+
+        return false;        
+    }
 };
