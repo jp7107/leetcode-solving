98f214b1d859387442c8607cde97c985a3050f67
Jyoti Prakash <jpjangid710@gmail.com>
2026-05-06 03:13:34 +0530

    Time: 5 ms (1.69%), Space: 9.5 MB (14.14%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,16 +1,38 @@
 class Solution {
 public:
-    char findTheDifference(string s, string t) {
-        int n = s.length();
-        // vector<char> cc;
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        for(int i=0; i<s.size(); i++){
-            if(s[i] != t[i]){
-                return t[i];
-            }
-        }
+//     char findTheDifference(string s, string t) {
+//         int n = s.length();
+//         // vector<char> cc;
+//         sort(s.begin(), s.end());
+//         sort(t.begin(), t.end());
+//         for(int i=0; i<s.size(); i++){
+//             if(s[i] != t[i]){
+//                 return t[i];
+//             }
+//         }
         
-        return t[n];
+//         return t[n];
+//     }
+char findTheDifference(string s, string t) {
+    vector<int> freq(26, 0);
+
+    // Count characters in s
+    for (char c : s) {
+        freq[c - 'a']++;
     }
+
+    // Subtract using t
+    for (char c : t) {
+        freq[c - 'a']--;
+    }
+
+    // Find the extra character
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] != 0) {
+            return i + 'a';
+        }
+    }
+
+    return '#'; // fallback (should not happen)
+}
 };
