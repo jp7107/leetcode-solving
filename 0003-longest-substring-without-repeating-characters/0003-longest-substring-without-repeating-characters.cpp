13dc248902fb4ce840705ab4aa910252d1a72127
Jyoti Prakash <jpjangid710@gmail.com>
2026-05-07 01:06:47 +0530

    Time: 687 ms (5%), Space: 247.5 MB (5.25%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        int maxi= 0;//we can use INT_MIN instead of 0 for managing neagative number 
+        for(int i=0; i<n; i++){
+            int cnt = 0;
+            unordered_map<char, int > hakla;
+
+            for(int j=i; j<n; j++){
+                //store inside hakla map
+                hakla[s[j]]++;
+                //check the condition
+                if(hakla[s[j]] > 1)
+                break;
+
+                cnt++;
+                maxi= max(maxi, cnt);
+            }
+
+
+        }
+        return maxi;
+    }  
+};
