c98d55a26530e0ac0650df7960052776030d603e
Jyoti Prakash <jpjangid710@gmail.com>
2026-05-07 23:42:41 +0530

    Time: 0 ms (100%), Space: 8.6 MB (32.01%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        
+        int cnt = 0;
+
+        for(int i = 0; i < t.length(); i++) {
+            if(cnt < s.length() && s[cnt] == t[i]) {
+                cnt++;
+            }
+        }
+
+        return cnt == s.length() ?true:false;
+    }
+};
