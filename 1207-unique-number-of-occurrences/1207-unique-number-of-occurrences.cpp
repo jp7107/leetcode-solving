4aaaece6b8268bf3fd60e1f9144034e3fecea842
Jyoti Prakash <jpjangid710@gmail.com>
2026-06-08 08:40:07 +0530

    Time: 3 ms (15.58%), Space: 11.8 MB (92.07%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,24 +1,53 @@
+// class Solution {
+// public:
+//     bool uniqueOccurrences(vector<int>& arr) {
+//         unordered_map<int,int>map;
+//         unordered_map<int,int>map2;
+//         for(auto x: arr){
+//             map[x]++;
+
+//         }
+//         for(auto n: map){
+//             int val=n.second;
+
+//             map2[val]++;
+           
+//         }
+//         int cnt=0;
+//         for(auto c:map2){
+//             if(c.second ==1){
+//                 cnt++;
+//             }
+//         }
+//         return cnt==map2.size();
+//     }
+// };
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int,int>map;
-        unordered_map<int,int>map2;
-        for(auto x: arr){
-            map[x]++;
+        sort(arr.begin(), arr.end());
+        vector<int> v;
 
-        }
-        for(auto n: map){
-            int val=n.second;
+        for (int i = 0; i < arr.size(); i++) {
+            int cnt = 1;
 
-            map2[val]++;
-           
-        }
-        int cnt=0;
-        for(auto c:map2){
-            if(c.second ==1){
+            // Count occurrences of the current element
+            while (i + 1 < arr.size() && arr[i] == arr[i + 1]) {
                 cnt++;
+                i++;
+            }
+
+            v.push_back(cnt);
+        }
+
+        sort(v.begin(), v.end());
+
+        for (int i = 1; i < v.size(); i++) {
+            if (v[i] == v[i - 1]) {
+                return false;
             }
         }
-        return cnt==map2.size();
+
+        return true;
     }
-};
+}; 
