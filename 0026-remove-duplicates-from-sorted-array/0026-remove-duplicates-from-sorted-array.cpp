746008b40bea8736f1a8f6c9e37728c920db1ccc
Jyoti Prakash <jpjangid710@gmail.com>
2026-07-01 01:03:49 +0530

    Time: 7 ms (5.58%), Space: 23.7 MB (6.26%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,15 +1,37 @@
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         if(nums.size() == 0) return 0;
+
+//         int j = 1;
+//         for(int i = 1; i < nums.size(); i++) {
+//             if(nums[i] != nums[i - 1]) {
+//                 nums[j] = nums[i];
+//                 j++;
+//             }
+//         }
+//         return j;
+//     }
+// };
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.size() == 0) return 0;
+        unordered_set<int> s;
+        vector<int> temp;
 
-        int j = 1;
-        for(int i = 1; i < nums.size(); i++) {
-            if(nums[i] != nums[i - 1]) {
-                nums[j] = nums[i];
-                j++;
+        // Insert unique elements using hashing
+        for(int x : nums) {
+            if(s.find(x) == s.end()) {  
+                s.insert(x);
+                temp.push_back(x);
             }
         }
-        return j;
+
+        // Copy back unique elements into nums
+        for(int i = 0; i < temp.size(); i++) {
+            nums[i] = temp[i];
+        }
+
+        return temp.size();  // number of unique elements
     }
 };
