dcddde968508d8e1718ac2d84d935754cf061972
Jyoti Prakash <jpjangid710@gmail.com>
2026-07-17 03:11:33 +0530

    Time: 0 ms (100%), Space: 8.5 MB (57.14%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,17 +1,34 @@
-
 class Solution {
 public:
     string makeGood(string s) {
         string result = "";
 
         for (char ch : s) {
-            if (!result.empty() && abs(result.back() - ch) == 32) {
-                result.pop_back();   // remove bad pair
+            if (!result.empty() && 
+               (result.back() + 32 == ch || result.back() - 32 == ch)) {
+                result.pop_back();
             } else {
-                result.push_back(ch); // keep character
+                result.push_back(ch);
             }
         }
 
         return result;
+        /* ----------using stack--------------*/
+        // stack<char> stack;
+        // for (char c : s) {
+        //     if (!stack.empty() && abs(c - stack.top()) == 32) {
+        //         stack.pop();
+        //     } else {
+        //         stack.push(c);
+        //     }
+        // }
+        
+        // string result;
+        // while (!stack.empty()) {
+        //     result = stack.top() + result;
+        //     stack.pop();
+        // }
+        
+        // return result;
     }
 };
