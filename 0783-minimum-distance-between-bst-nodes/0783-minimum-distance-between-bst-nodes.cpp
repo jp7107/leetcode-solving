abb337d133ce9a22e08dc294f5106649f8e6afba
Jyoti Prakash <jpjangid710@gmail.com>
2026-07-26 01:45:55 +0530

    Time: 0 ms (100%), Space: 12.5 MB (58.94%) - LeetHub

---
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int getMax(TreeNode* root) {
+        while(root->right != nullptr) {
+            root = root->right;
+        }
+        return root->val;
+    }
+
+    int getMin(TreeNode* root) {
+        while(root->left != nullptr) {
+            root = root->left;
+        }
+        return root->val;
+    }
+
+    int minDiffInBST(TreeNode* root) {
+        if(root == nullptr)
+            return INT_MAX;
+
+        int lowestNumber = INT_MAX;
+
+        if(root->left != nullptr) {
+            lowestNumber = min(lowestNumber,
+                               root->val - getMax(root->left));
+        }
+
+        if(root->right != nullptr) {
+            lowestNumber = min(lowestNumber,
+                               getMin(root->right) - root->val);
+        }
+
+        int L = minDiffInBST(root->left);
+        int R = minDiffInBST(root->right);
+
+        lowestNumber = min(lowestNumber, L);
+        lowestNumber = min(lowestNumber, R);
+
+        return lowestNumber;
+    }
+};
