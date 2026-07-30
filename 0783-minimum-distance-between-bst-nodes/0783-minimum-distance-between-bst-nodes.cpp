3590f222da0ed904634f47fec009ef6d0e4fdbda
Jyoti Prakash <jpjangid710@gmail.com>
2026-07-30 18:49:25 +0530

    Time: 0 ms (100%), Space: 12.6 MB (23.76%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
@@ -8,52 +8,47 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- */
-// class Solution {
-// public:
-//    int getMax(TreeNode* root) {
-//         while(root->right != nullptr) {
-//             root = root->right;
-//         }
-//         return root->val;
-//     }
-//     int getMin(TreeNode* root) {
-//         while(root->left != nullptr) {
-//             root = root->left;
-//         }
-//         return root->val;
-//     }
-//     int minDiffInBST(TreeNode* root) {
-//         if(root == nullptr)
-//             return INT_MAX;
+  */
 
-//         int lowestNumber = INT_MAX;
-
-//         if(root->left != nullptr) {
-//             lowestNumber = min(lowestNumber, root->val - getMax(root->left));
-//         }
-
-//         if(root->right != nullptr) {
-//             lowestNumber = min(lowestNumber, getMin(root->right) - root->val);
-//         }
-
-//         int L = minDiffInBST(root->left);
-//         int R = minDiffInBST(root->right);
-
-//         lowestNumber = min(lowestNumber, L);
-//         lowestNumber = min(lowestNumber, R);
-
-//         return lowestNumber;
-//     }
-// };
 class Solution {
   public:
-    int res = INT_MAX, pre = -1;
+    int ans = INT_MAX, pre = -1;
     int minDiffInBST(TreeNode* root) {
-        if (root->left != NULL) minDiffInBST(root->left);
-        if (pre >= 0) res = min(res, root->val - pre);
+        //base1
+        if (root->left != NULL){
+         minDiffInBST(root->left);
+        }    
+        //base2 
+        if (pre >= 0){
+             ans = min(ans, root->val - pre);
+        }
+        //main code logic
         pre = root->val;
+        //for right cuz doing Inorder left->root->right
         if (root->right != NULL) minDiffInBST(root->right);
-        return res;
+        return ans;
     }
 };
+// class Solution {
+// public:
+//     void inorder(TreeNode* root, vector<int>& arr) {
+//         if (root == NULL) return;
+
+//         inorder(root->left, arr);
+//         arr.push_back(root->val);
+//         inorder(root->right, arr);
+//     }
+
+//     int minDiffInBST(TreeNode* root) {
+//         vector<int> arr;
+//         inorder(root, arr);
+
+//         int ans = INT_MAX;
+
+//         for (int i = 1; i < arr.size(); i++) {
+//             ans = min(ans, arr[i] - arr[i - 1]);
+//         }
+
+//         return ans;
+//     }
+// };
